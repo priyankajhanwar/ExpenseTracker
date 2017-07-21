70080012e166c716224045aba3128dd927f1a3fb
priyankajhanwar <pjhanwar22@gmail.com>
2017-07-21 11:55:28 +0530

    data ready in percents

--- src/app/mock-expense.ts
+++ src/app/mock-expense.ts
@@ -4,13 +4,25 @@ export const EXPENSES: Expense[] = [
 {
 	spendFor : 'Coffee',
 	amountSpend : 50,
-	paymentDate : new Date(1988, 3, 15),
+	paymentDate : new Date(2000, 3, 15),
 	spendOn:'Bill'
 },
 {
-	spendFor : 'Parking',
-	amountSpend : 100,
+	spendFor : 'Chocolates',
+	amountSpend : 50,
+	paymentDate : new Date(2008, 3, 15),
+	spendOn:'FoodDrink'
+},
+{
+	spendFor : 'LIC',
+	amountSpend : 9000,
 	paymentDate : new Date(1988, 3, 15),
+	spendOn:'Investment'
+},
+{
+	spendFor : 'PVR',
+	amountSpend : 900,
+	paymentDate : new Date(2017, 3, 15),
 	spendOn:'Entertainment'
 }
 ];
--- src/app/spents.service.ts
+++ src/app/spents.service.ts
@@ -9,40 +9,87 @@ export class SpentsService {
 
   constructor(private router: Router) { }
 
-  updatedExpensesList : Expense[];
-  afterRemovalList : Expense[];
-  // path:any;
   getData():Expense[]{
-  	return EXPENSES;
+    return EXPENSES;
   }
 
   addNewExpense(description,amount,date,category): Expense[]{
-  	this.updatedExpensesList = this.getData();
-  	this.updatedExpensesList.push({
-			spendFor: description,
-			amountSpend: amount,
-			paymentDate: date,
+    let updatedExpensesList = this.getData();
+    updatedExpensesList.push({
+      spendFor: description,
+      amountSpend: amount,
+      paymentDate: date,
       spendOn : category
-	});
-
+    });
     this.router.navigate(['']);
-    console.log(this.getData());
-    console.log(this.updatedExpensesList);
-	  return this.updatedExpensesList;
+    // console.log("after adding");
+    // console.log(updatedExpensesList);
+    // console.log("category expenses");
+    // console.log(this.categoryTotals());
+    // console.log("total expense");
+    // console.log(this.totalExpenses());
+    console.log(this.percentCalculations());
+    return updatedExpensesList;
   }
 
   remove(popExpense): Expense[]{
-    this.afterRemovalList = this.getData();
-    this.afterRemovalList.splice(popExpense,1);
-    return this.afterRemovalList;
+    let afterRemovalList = this.getData();
+    afterRemovalList.splice(popExpense,1);
+    // console.log("after removal");
+    // console.log(afterRemovalList);
+    // console.log("category expenses");
+    // console.log(this.categoryTotals());
+    // console.log("total expense");
+    // console.log(this.totalExpenses());
+     console.log(this.percentCalculations());
+    return afterRemovalList;
   }
 
-  // getImages(){[
-  //   this.path : "../../img/billIcon.png",
-  //    path :"../../img/entertainment.png",
-  //    path :"../../img/foodDrink.jpeg",
-  //   path:"../../img/investment.jpeg"
-  // ]}
-  
+  totalExpenses():number{
+    let finalBill=0;
+    let array=[];
+    array=this.categoryTotals();
+    for(let i=0;i<array.length;i++){
+      finalBill = finalBill + array[i] ;
+    }
+    console.log(finalBill);
+    return finalBill;
+  }
+
+  categoryTotals():Array<number>{
+    let list = this.getData();
+    let billsTotal=0;
+    let investmentTotal=0;
+    let entertainmentTotal=0;
+    let foodTotal=0;
+    for(let i=0;i<list.length;i++){
+      if(list[i].spendOn==='Bill'){
+        billsTotal = billsTotal + list[i].amountSpend ;
+      }
+      if(list[i].spendOn==='Investment'){
+        investmentTotal = investmentTotal + list[i].amountSpend ;
+      }
+      if(list[i].spendOn==='Entertainment'){
+        entertainmentTotal = entertainmentTotal + list[i].amountSpend ;
+      }
+      if(list[i].spendOn==='FoodDrink'){
+        foodTotal = foodTotal + list[i].amountSpend ;
+      }  
+    }
+    console.log("bills" + billsTotal);console.log("invests"+ investmentTotal);console.log("fun"+entertainmentTotal);console.log("food"+foodTotal);
+    return [billsTotal,investmentTotal,entertainmentTotal,foodTotal]; 
+  }
+
+  percentCalculations():Array<number>{
+    let categorySpents=this.categoryTotals();
+    let finalExpense=this.totalExpenses(); 
+    let percentArray=[];
+     for(let i=0;i<categorySpents.length;i++){
+       percentArray[i]=(categorySpents[i]/finalExpense)*100;
+       console.log(percentArray[i]+"%")
+     }
+
+    return percentArray;
+  }
 
 }
